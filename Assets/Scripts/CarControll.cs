0d7be1366937d34b902716d3ca40eadd2e88d680
Kratoo5 <kratos666666@gmail.com>
2015-08-09 11:22:06 +0300

    first commit. Connection and pos sync

---
+++ Assets/Scripts/CarControll.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControll : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float rotateSpeed = 20;
+    public float speed = 10;
+    public Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        cam.transform.LookAt(transform);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector3.right * speed);
+    }
+}
---
+++ Assets/Scripts/PlayerStuff.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerStuff : NetworkBehaviour{
+
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController player;
+    public CharacterController playerController;
+
+    public Camera FPSCam;
+    public AudioListener audioListener;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("SceneCamera").SetActive(false);
+		if (isLocalPlayer){
+            player.enabled = true;
+            playerController.enabled = true;
+            FPSCam.enabled = true;
+            audioListener.enabled = true;
+        }
+        else
+        {
+            player.enabled = false;
+            playerController.enabled = false;
+            FPSCam.enabled = false;
+            audioListener.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
